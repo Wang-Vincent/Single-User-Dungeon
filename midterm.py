975ca7b8b4b486ff1419e4c57ac7ec69813bf966
vwparadox <58446534+vwparadox@users.noreply.github.com>
2020-10-23 19:50:54 +0000

    Vincent health_regen()

--- midterm.py
+++ midterm.py
@@ -86,10 +86,10 @@ def move_character(character, direction):
 
     Move character's current position by 1 towards given direction
 
-    :param character: a one dimensional list
-    :param direction: an integer represent a specific direction, 1 = north, 2 = south, 3 = west, 4 = east
+    :param character: a one dimensional list containing the character's current status
+    :param direction: an integer representing a specific direction, 1 = north, 2 = south, 3 = west, 4 = east
     :precondition: character list must contains character's current x and y position, its heal and name in order;
-    direction must one of 1, 2, 3, 4
+    direction must be one of 1, 2, 3, 4
     :postcondition: move character's position by one towards correct direction
     :return: None
 
@@ -121,3 +121,84 @@ def move_character(character, direction):
         character[0] -= 1
     elif direction == 4:
         character[0] += 1
+
+
+def validate_and_move(character, direction, board):
+    """Validate a move and move a character
+
+    Validate if a move is valid, perform the move if it is valid
+
+    :param character: a one dimensional list containing the character's current status
+    :param direction: an integer representing a specific direction, 1 = north, 2 = south, 3 = west, 4 = east
+    :param board: a two dimensional list representing the whole dungeon
+    :precondition: character list must contains character's current x and y position, its heal and name in order;
+    direction must be one of 1, 2, 3, 4; board has to be a list which contains five [1,2,3,4,5] as its element.
+    :postcondition: Correctly validate a move; perform the corresponding move if it is valid
+    :return: True if the move is valid; Otherwise reture False
+
+    >>> test_board = [[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5]]
+    >>> test_player = [3,5,3,'okopogo']
+    >>> validate_and_move(test_player, 1, test_board)
+    False
+    >>> test_board = [[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5]]
+    >>> test_player = [3,5,3,'okopogo']
+    >>> validate_and_move(test_player, 2, test_board)
+    True
+    >>> test_board = [[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5]]
+    >>> test_player = [3,5,3,'okopogo']
+    >>> validate_and_move(test_player, 3, test_board)
+    False
+    >>> test_board = [[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5]]
+    >>> test_player = [3,5,3,'okopogo']
+    >>> validate_and_move(test_player, 4, test_board)
+    True
+    >>> test_board = [[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5]]
+    >>> test_player = [3,5,3,'okopogo']
+    >>> validate_and_move(test_player, 'wasd', test_board)
+    Sorry, I can't understand you.
+    False
+    """
+
+    directions = (1, 2, 3, 4)
+    if direction in directions:
+        initial_position_x = character[0]
+        initial_position_y = character[1]
+        move_character(character, direction)
+        if character[0] not in range(1, len(board[0]) + 1):
+            character[0] = initial_position_x
+            return False
+        elif character[1] not in range(1, len(board) + 1):
+            character[1] = initial_position_y
+            return False
+        else:
+            return True
+    else:
+        print("Sorry, I can't understand you.")
+        return False
+
+
+def health_regen(character):
+    """
+    Regenerate Player's health points if they are in range(1, 9).
+    
+    :param character: a 1D list representing player's status
+    :precondition: character[2], aka player's health, must be in range(1, 9)
+    :postcondition: player's HP will +2 if they are in range(1, 8),+1 if they are 9
+    :return: None
+    >>> test_character = [2, 3, 6, 'test_player']   
+    >>> health_regen(test_character)
+    >>> print(test_character)
+    [2, 3, 8, 'test_player']
+    >>> test_character = [2, 3, 9, 'test_player']   
+    >>> health_regen(test_character)
+    >>> print(test_character)
+    [2, 3, 10, 'test_player'] 
+    >>> test_character = [2, 3, 10, 'test_player']   
+    >>> health_regen(test_character)
+    >>> print(test_character)
+    [2, 3, 10, 'test_player']  
+    """
+    if 1 <= character[2] <= 8:
+        character[2] += 2
+    elif character[2] == 9:
+        character[2] = 10
